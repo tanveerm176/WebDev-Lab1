ffd4da2c54074f0292923580a8370c4371a78b11
tanveerm176 <tanveerm176@gmail.com>
2024-04-28 12:47:43 -0400

    created table

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Calendar from './components/Calendar'
 
 const App = () =>{
   return(
     <div className='App'>
       <h1>Iternerary for 7 Days in New York</h1>
       <h2>Welcome to New York, Mo! Check out this calendar to get to know the city better!</h2>
-
+      <Calendar />
     </div>
   )
 }
